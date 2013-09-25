930c9c7341af2955ea87f9d2be2c95fea32f7657
Andrew Keedle <andrew.keedle@btsgroupuk.com>
2013-09-25 09:11:23 +0100

    update core.asyc dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1859"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
                  [org.clojure/core.match "0.2.0-rc6"]
                  [prismatic/dommy "0.1.2"]
                  [com.keminglabs/reflex "0.1.1"]]
